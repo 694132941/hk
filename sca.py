47264613c784d4f43e78c1ea493066373abd089d
root <root@localhost.localdomain>
2017-11-27 05:10:47 +0800

    20183252343

--- sca.py
+++ sca.py
@@ -23,4 +23,24 @@ def portScan(tgtHost, tgtPorts):
 	return
     try:
 	tgtName = socket.gethostbyaddr(tgtIP)
-	 
+	print('\n[+] Scan Results for:' + tgtName[0]) 
+    except:
+	print('\n[+] Scan Results for:' + tgtIP)
+    socket.setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+	print('Scanning port ' + str(tgtPort))
+	connScan(tgtHost, int(tgtPort))
+def main():
+    parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
+    parser.add_option('-H',dest='tgtHost',type='string',help='specify target host')
+    parser.add_option('-p',dest='tgtPort',type='int',help='specify target port')
+    (options,args)= parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPort = options.tgtPort
+    if(tgtHost == None)|(tgtPort == None):
+	print('[-]You must specify a target host and port[s]!')
+	exit(0) 
+    portScan(tgtHost,args)
+if __name__ == '__main__':
+    main()
+    
